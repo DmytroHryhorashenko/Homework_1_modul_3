555aff3c1d276fc967e73aa4497afdd2daba0e26
DmytroHryhorashenko <dimon09051991@gmail.com>
2026-02-05 16:53:21 +0200

    .git

--- HomeWork_1.py
+++ HomeWork_1.py
@@ -12,54 +12,5 @@ def get_days_from_today(date_str):
 print(get_days_from_today("2021-10-09"))
 
 
-# 2. Функція get_numbers_ticket
-import random
-
-def get_numbers_ticket(min_val, max_val, quantity):
-    if not (1 <= min_val <= max_val <= 1000):
-        return []
-    if not (min_val <= quantity <= (max_val - min_val + 1)):
-        return []
-    numbers = set()
-    while len(numbers) < quantity:
-        numbers.add(random.randint(min_val, max_val))
-    return sorted(numbers)
-lottery_numbers = get_numbers_ticket(1, 49, 6)
-print("Ваші лотерейні числа:", lottery_numbers)
 
 
-
-# 3.Функція normalize_phone(необов’язкове)
-
-import re
-
-def normalize_phone(phone_number):
-    phone_number = phone_number.strip()
-    phone_number = re.sub(r"[^\d+]", "", phone_number)
-
-    if phone_number.startswith("+"):
-        if not phone_number.startswith("+38"):
-            if phone_number.startswith("+380"):
-                pass
-            else:
-                phone_number = "+38" + phone_number[1:]
-    else:
-        if phone_number.startswith("380"):
-            phone_number = "+" + phone_number
-        else:
-            phone_number = "+38" + phone_number
-    return phone_number
-raw_numbers = [
-    "067\t123 4567",
-    "(095) 234-5678\n",
-    "+380 44 123 4567",
-    "380501234567",
-    "    +38(050)123-32-34",
-    "     0503451234",
-    "(050)8889900",
-    "38050-111-22-22",
-    "38050 111 22 11   ",
-]
-
-sanitized_numbers = [normalize_phone(num) for num in raw_numbers]
-print("Нормалізовані номери телефонів для SMS-розсилки:", sanitized_numbers)
---
+++ HomeWork_1_2.py
@@ -0,0 +1,14 @@
+# 2. Функція get_numbers_ticket
+import random
+
+def get_numbers_ticket(min_val, max_val, quantity):
+    if not (1 <= min_val <= max_val <= 1000):
+        return []
+    if not (min_val <= quantity <= (max_val - min_val + 1)):
+        return []
+    numbers = set()
+    while len(numbers) < quantity:
+        numbers.add(random.randint(min_val, max_val))
+    return sorted(numbers)
+lottery_numbers = get_numbers_ticket(1, 49, 6)
+print("Ваші лотерейні числа:", lottery_numbers)
